14f682f4fd6b50b25083a06e4c2af9062f177ad5
dennyh89 <denny.herbrich@gmail.com>
2016-09-12 16:22:43 +0200

    initial commit Auth Server to run locally

---
+++ src/main/java/com/spring/cloud/ref/AuthServerApplication.java
@@ -0,0 +1,33 @@
+package com.spring.cloud.ref;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class AuthServerApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+	
+	
+	
+}
