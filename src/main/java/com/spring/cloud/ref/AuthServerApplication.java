0001685e8edda0be627444b22d4abf7a48c619c6
Denny Herbrich <denny.herbrich@gmail.com>
2017-06-10 23:38:30 +0200

    Test-2 implementation

--- src/main/java/com/spring/cloud/ref/AuthServerApplication.java
+++ src/main/java/com/spring/cloud/ref/AuthServerApplication.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class AuthServerApplication {
 //Test-1
+//Test2
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
 		return user;
